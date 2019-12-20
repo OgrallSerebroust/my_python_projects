b90cced5b1c7576b9f53a14156d54800ecc994c6
Ograll <stefan.klepikov@mail.ru>
2019-12-21 02:14:07 +0300

    rulles_complete

--- core.py
+++ core.py
@@ -1,3 +1,7 @@
+first_or_second = False
+key_list, value_list = [], []
+our_rules = {}
+key, value = "", ""
 print("Дорогой пользователь, пожалуйста, задайте грамматику G...\n")
 count_of_neterminals = list(input("Для этого задайте нетерминалы грамматики(ОБЯЗАТЕЛЕН К ЗАДАНИЮ S!): "))
 for i in range(int(len(count_of_neterminals) / 2)):
@@ -6,4 +10,25 @@ count_of_terminals = list(input("Кроме этого, не забудьте з
 for i in range(int(len(count_of_terminals) / 2)):
     count_of_terminals.remove(" ")
 print(count_of_neterminals, count_of_terminals)
-print("Теперь можно задать правила грамматики...\nПожалуйста, следуйте примеру 'S --> Aa'.")
+print("Теперь можно задать правила грамматики(Чтобы прекратить введите пустую строку)\n")
+rule = str(input("Пожалуйста, следуйте примеру 'S --> Aa'.\n"))
+while rule != "":
+    for i in rule:
+        if i != " " and i != "-" and i != ">" and first_or_second == False:
+            key_list.append(i)
+        if i == ">":
+            first_or_second = True
+        if i != " " and i != "-" and i != ">" and first_or_second == True:
+            value_list.append(i)
+    print(key_list, value_list)
+    for i in range(len(key_list)):
+        key += key_list[i]
+    for i in range(len(value_list)):
+        value += value_list[i]
+    print(key, value)
+    our_rules.update({key:value})
+    print(our_rules)
+    first_or_second = False
+    key_list, value_list = [], []
+    key, value = "", ""
+    rule = str(input("Пожалуйста, следуйте примеру 'S --> Aa'.\n"))
