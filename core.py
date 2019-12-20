f7ac8557cc38bf893afedcb156dd1bf8f65848b0
Ograll <stefan.klepikov@mail.ru>
2019-12-21 01:33:56 +0300

    start_of_adding_rules

--- core.py
+++ core.py
@@ -1,5 +1,9 @@
 print("Дорогой пользователь, пожалуйста, задайте грамматику G...\n")
-N = list(input("Для этого задайте нетерминалы грамматики: "))
-for i in range(int(len(N) / 2)):
-    N.remove(" ")
-print(N)
+count_of_neterminals = list(input("Для этого задайте нетерминалы грамматики(ОБЯЗАТЕЛЕН К ЗАДАНИЮ S!): "))
+for i in range(int(len(count_of_neterminals) / 2)):
+    count_of_neterminals.remove(" ")
+count_of_terminals = list(input("Кроме этого, не забудьте задать терминалы грамматики: "))
+for i in range(int(len(count_of_terminals) / 2)):
+    count_of_terminals.remove(" ")
+print(count_of_neterminals, count_of_terminals)
+print("Теперь можно задать правила грамматики...\nПожалуйста, следуйте примеру 'S --> Aa'.")
