2159619350a943a04d4a2a926f7d1781ce99d0f6
Ograll <stefan.klepikov@mail.ru>
2020-03-11 01:37:22 +0300

    Upgraded_resolution_of_videos_on_max

--- video_downloader/core.py
+++ video_downloader/core.py
@@ -1,6 +1,7 @@
 import sys
 import youtube_dl
-#from pytube import YouTube
+import os
+from moviepy.editor import VideoFileClip, AudioFileClip, concatenate_videoclips
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QHBoxLayout, QVBoxLayout, QFileDialog
 from PyQt5.QtGui import QIcon
 
@@ -33,19 +34,31 @@ class Our_gui(QWidget):
         vbox.addLayout(hbox_3)
         self.setLayout(vbox)
         self.setWindowTitle("YouTube video downloader")
-        self.setWindowIcon(QIcon("oformlenie/img/window_icon_V1.png"))
+        self.setWindowIcon(QIcon("media/img/window_icon_V1.png"))
 
     def Browse_of_destination(self):
         name_of_destination = QFileDialog.getExistingDirectory(None, "Select the destination", "\home")
         self.our_destination.setText(name_of_destination)
-
+    
     def Confirm_downloading(self):
-        our_youtube_link, our_completed_destination = self.our_link.text(), self.our_destination.text()
+        our_youtube_link, our_completed_destination, our_temp_destination = self.our_link.text(), self.our_destination.text(), "media/video/"
         if(our_youtube_link):
-            Location = '%s \%(extractor)s-%(id)s-%(title)s.%(ext)s'.replace("%s ", our_completed_destination)
-            ytdl_format_options = {'outtmpl': Location, "format": "best"}
+            Location = '%s \%(extractor)s-%(id)s-%(title)s.%(ext)s'.replace("%s ", our_temp_destination)
+            ytdl_format_options = {'outtmpl': Location, "format": "bestvideo + bestaudio"}
             with youtube_dl.YoutubeDL(ytdl_format_options) as ydl:
                 ydl.download([our_youtube_link])
+                self.Complete_downloading(our_completed_destination)
+
+    def Complete_downloading(self, our_completed_destination):
+        os.chdir("media/video/")
+        video = VideoFileClip(os.listdir()[1])
+        audio = AudioFileClip(os.listdir()[0])
+        new_video = video.set_audio(audio)
+        new_video.write_videofile(our_completed_destination + "/completed.mp4")
+        os.remove(os.listdir()[1])
+        os.remove(os.listdir()[0])
+        print("OK!")
+        
 if __name__ == "__main__":
     our_gui_application = QApplication(sys.argv)
     program = Our_gui()
