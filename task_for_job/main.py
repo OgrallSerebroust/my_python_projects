e36871b71ba558edf4df72744b33e91ea4162e51
Ograll <stefan.klepikov@mail.ru>
2020-12-03 15:24:16 +0300

    Saved_job_project

--- task_for_job/main.py
+++ task_for_job/main.py
@@ -3,16 +3,23 @@ from json import loads
 from os import mkdir
 
 if __name__ == '__main__':
-    name_of_person = ""
-    our_test_query = get("https://json.medrating.org/users", params={"id": "1"})
-    one_test_person = loads(our_test_query.text)
-    for _ in one_test_person:
-        if _["name"]:
-            name_of_person = _["name"]
-    try:
-        file_about_person = open("tasks/" + str(name_of_person) + ".txt", "w")
-        file_about_person.write(name_of_person)
-    except FileNotFoundError:
-        mkdir("tasks")
-        file_about_person = open("tasks/" + str(name_of_person) + ".txt", "w")
-        file_about_person.write(name_of_person)
+    def make_file_about_person(name, email):
+        file_about_person = open("tasks/" + str(name) + ".txt", "w")
+        file_about_person.write(name + " <" + email + ">")
+
+    def collecting_information_from_json(our_persons):
+        for _ in our_persons:
+            try:
+                name_of_person = _["name"]
+                email_of_person = _["email"]
+                try:
+                    make_file_about_person(name_of_person, email_of_person)
+                except FileNotFoundError:
+                    mkdir("tasks")
+                    make_file_about_person(name_of_person, email_of_person)
+            except KeyError:
+                print("")
+
+    our_query_for_all_information_from_json = get("https://json.medrating.org/users")
+    our_persons_from_query = loads(our_query_for_all_information_from_json.text)
+    collecting_information_from_json(our_persons_from_query)
