b55e408b79fad6d1d5b45ed973d4211d5984dd13
Ograll <stefan.klepikov@mail.ru>
2020-12-08 06:58:21 +0300

    Final_version_of_job_task

--- task_for_job/main.py
+++ task_for_job/main.py
@@ -1,18 +1,64 @@
 from requests import get
 from json import loads
-from os import mkdir
+from os import mkdir, path, rename
 from datetime import datetime
 
 if __name__ == '__main__':
+    def check_files_is_same(new_name, new_email, new_company, new_completed_tasks, new_not_completed_tasks):
+        old_file = open("tasks/" + str(new_name) + ".txt", "r", encoding="utf-8")
+        name_mail_and_time_line = old_file.readline()
+        email_from_file = name_mail_and_time_line.split()[-3::1][0]
+        name_from_file = str(name_mail_and_time_line.partition(email_from_file)[0][:-1])
+        company_from_file = old_file.readline().rstrip('\n')
+        old_file.readline()
+        i = old_file.readline()
+        completed_tasks_from_file, not_completed_tasks_from_file = [], []
+        while i != '\n':
+            i = old_file.readline()
+            if i != '\n':
+                completed_tasks_from_file.append(i[:-1])
+        j = old_file.readline()
+        while j != '':
+            j = old_file.readline()
+            if j != '':
+                not_completed_tasks_from_file.append(j[:-1])
+        old_file.close()
+        temp_completed_tasks_from_file = sorted(completed_tasks_from_file)
+        temp_new_completed_tasks = sorted(new_completed_tasks)
+        temp_not_completed_tasks_from_file = sorted(not_completed_tasks_from_file)
+        temp_new_not_completed_tasks = sorted(new_not_completed_tasks)
+        email_from_file = email_from_file[1:-1]
+        if new_name == name_from_file and new_email == email_from_file and new_company == company_from_file and temp_new_completed_tasks == temp_completed_tasks_from_file and temp_new_not_completed_tasks == temp_not_completed_tasks_from_file:
+            return True
+        else:
+            return False
+
+    def file_creator_function(name_for_creating, email_for_creating, time_for_creating, company_for_creating, completed_tasks_for_creating, not_completed_tasks_for_creating):
+        with open("tasks/" + str(name_for_creating) + ".txt", "w", encoding="utf-8") as file_about_person:
+            file_about_person.write(
+                name_for_creating + " <" + email_for_creating + "> " + time_for_creating + "\n" + company_for_creating + "\n\n" + "Завершённые задачи:\n")
+            for i in range(len(completed_tasks_for_creating)):
+                file_about_person.write(completed_tasks_for_creating[i] + "\n")
+            file_about_person.write("\nОставшиеся задачи:\n")
+            for j in range(len(not_completed_tasks_for_creating)):
+                file_about_person.write(not_completed_tasks_for_creating[j] + "\n")
+            file_about_person.close()
+
     def make_file_about_person(name, email, time, company, completed_tasks, not_completed_tasks):
-        file_about_person = open("tasks/" + str(name) + ".txt", "w", encoding="utf-8")
-        file_about_person.write(name + " <" + email + "> " + time + "\n" + company + "\n\n" + "Завершённые задачи:\n")
-        for i in range(len(completed_tasks)):
-            file_about_person.write(completed_tasks[i] + "\n")
-        file_about_person.write("\nОставшиеся задачи:\n")
-        for j in range(len(not_completed_tasks)):
-            file_about_person.write(not_completed_tasks[j] + "\n")
-        file_about_person.close()
+        if path.exists("tasks/" + str(name) + ".txt"):
+            if not check_files_is_same(name, email, company, completed_tasks, not_completed_tasks):
+                with open("tasks/" + str(name) + ".txt", "r", encoding="utf-8") as old_file:
+                    name_mail_and_time_line = old_file.readline()
+                    where_was_create_old_file = name_mail_and_time_line.split()[-2::]
+                rename("tasks/" + str(name) + ".txt",
+                       "tasks/" + str(name) + "_" + str(where_was_create_old_file[0].split(".")[2]) + "-" + str(
+                           where_was_create_old_file[0].split(".")[1]) + "-" + str(
+                           where_was_create_old_file[0].split(".")[0]) + "T" + str(
+                           where_was_create_old_file[1].split(":")[0]) + "-" + str(
+                           where_was_create_old_file[1].split(":")[1]) + ".txt")
+                file_creator_function(name, email, time, company, completed_tasks, not_completed_tasks)
+        else:
+            file_creator_function(name, email, time, company, completed_tasks, not_completed_tasks)
 
     def collecting_information_from_todos_json(user_id):
         completed_tasks, not_completed_tasks = [], []
@@ -60,8 +106,8 @@ if __name__ == '__main__':
     our_persons_from_query = loads(our_query_for_all_information_from_json.text)
     users_data_without_full_info_set = 0
     collecting_information_from_json(our_persons_from_query)
-    print("К сожалению не все данные были обработаны!\n" + str(users_data_without_full_info_set) + " пользователей имеют неполные данные!")
-    # TODO Если файл для пользователя уже существует, то существующий файл переименовать, добавив в него время составления этого старого отчёта в формате "Antonette_2020-09-23T15:25.txt" Таким образом, актуальный отчёт всегда будет без даты в названии. Старые отчёты не удаляются, а переименовываются.
+    print("К сожалению не все данные были обработаны!\n" + str(
+        users_data_without_full_info_set) + " пользователей имеют неполные данные!")
     # TODO Предусмотреть возможные сбои в сети или при записи на диск. Не должно быть наполовину сформированных файлов. Либо файл есть и он целиком корректный, либо его нет.
     # TODO Если по юзеру однажды был создан отчёт, то всегда должен существовать актуальный отчёт без даты в названии. Не должно быть такого, что из-за сбоя в сети или т.п. остались только файлы с датами в названиях.
     # TODO Если какие-то моменты не обговорены в задаче, то продумайте плюсы и минусы возможных вариантов, и выберите наиболее подходящий на ваш взгляд, чтобы потом можно было обосновать своё решение.
